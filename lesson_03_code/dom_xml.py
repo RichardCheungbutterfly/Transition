41a2e4a2d669a42275484436c8244608751431b8
Richard Cheung <32785611+RichardCheungbutterfly@users.noreply.github.com>
2018-05-03 11:46:00 +0800

    add lesson_03_04_codes

---
+++ lesson_03_code/dom_xml.py
@@ -0,0 +1,14 @@
+from xml.dom import minidom
+
+doc = minidom.parse('book.xml')
+root = doc.documentElement
+# print(dir(root))
+print(root.nodeName)
+books = root.getElementsByTagName('book')
+print(type(books))
+print(books)
+for book in books:
+    titles = book.getElementsByTagName('title')
+    print(titles[0].childNodes[0].nodeValue)
+    
+    
